6cb493c223e1f750e9d306c6d889c4d395192a6a
Anders Christensen <anders@medieskolerne.dk>
2023-09-10 20:55:59 +0200

    Added 2 Videos

--- src/index.mjs
+++ src/index.mjs
@@ -15,7 +15,14 @@ const application = {
                 chapter : "chapter-2",
                 duration : "15m.06s",
                 title : "Boiler tilpasning og setup.",
-                video : "https://player.vimeo.com/progressive_redirect/playback/862932854/rendition/1080p/file.mp4?loc=external&log_user=0&signature=b9db0ffe6d907eb348b293d936b207cd2aa61ce8017014e7fd12eb6bd5e1e60b",
+                video : "https://player.vimeo.com/progressive_redirect/playback/862937153/rendition/1080p/file.mp4?loc=external&log_user=0&signature=525382603764f8a72b9f1e6eef386bdaeb31a007ce91a99b8a1c7e3ae07ef209",
+                description : "Introduktion til at arbejde med NextJS."
+            },
+            {
+                chapter : "chapter-3",
+                duration : "13m.18s",
+                title : "Pages",
+                video : "https://player.vimeo.com/progressive_redirect/playback/862937167/rendition/1080p/file.mp4?loc=external&log_user=0&signature=2465deee271373c4ed86069ac3f3977a67566980bfde9d8963ddaf316322b67d",
                 description : "Introduktion til at arbejde med NextJS."
             }
         ]
