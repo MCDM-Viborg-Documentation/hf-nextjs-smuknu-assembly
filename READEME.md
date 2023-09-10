0999f2dae46564ea57bfe783fb322a22eac3e382
Anders Christensen <anders@medieskolerne.dk>
2023-09-10 20:37:04 +0200

    pages

---
+++ READEME.md
@@ -0,0 +1,24 @@
+# Step List
+
+## Step 01. - Setup
+
+* Video 01 - Intro.
+* Video 02 - Design.
+    * Boiler Cleanup og øndringer af font og lidt globale indstillinger.
+* Video 03 
+    * Looking at Page-Diagram
+    * Setting Up Pages and .page class.
+
+## Step 02. - Komponenter.
+
+* Video 04 - Thinking In ReactSetting Up Top and Footer
+* Video 05 - Navigation - useState open/close
+* Video 06 - Quick About Icons - Explaining Footer, Member and Navigation
+
+## Step 03. - Data og Produkter.
+
+* Video 07 - Postman, data service Products Component.
+* Video 08 - Products.
+* Video 09 - Product Headers.
+
+## Step 04. - Data og Produkter.
---
+++ src/index.mjs
@@ -0,0 +1,89 @@
+const application = {
+    
+    data : {
+        title : 'NextJS - SmukNu Run-Through',
+        description : '...',
+        videos : [
+            {
+                chapter : "chapter-1",
+                duration : "02m.02s",
+                title : "Intro",
+                video : "https://player.vimeo.com/progressive_redirect/playback/862932854/rendition/1080p/file.mp4?loc=external&log_user=0&signature=b9db0ffe6d907eb348b293d936b207cd2aa61ce8017014e7fd12eb6bd5e1e60b",
+                description : "Introduktion til at arbejde med NextJS."
+            },
+            {
+                chapter : "chapter-2",
+                duration : "15m.06s",
+                title : "Boiler tilpasning og setup.",
+                video : "https://player.vimeo.com/progressive_redirect/playback/862932854/rendition/1080p/file.mp4?loc=external&log_user=0&signature=b9db0ffe6d907eb348b293d936b207cd2aa61ce8017014e7fd12eb6bd5e1e60b",
+                description : "Introduktion til at arbejde med NextJS."
+            }
+        ]
+    },
+
+    bodyTmpl : (location) => {
+
+        let applicationRoot =  'hf-nextjs-intro-boilerplate';
+        let gitHubPageIODomain = 'mcdm-viborg-documentation.github.io';
+
+        let path = (location.host === gitHubPageIODomain) ? location.origin + '/' + applicationRoot + '/' : '/';
+
+        return `<div>
+            <h1><img src="./assets/square_logo.png" width="50px">${application.data.title}</h1>
+            <p>${application.data.description}</p>
+            <h3>Index</h3>
+            <ol>
+                ${application.data.videos.map((video, index) => { return `<li><a href="${path}?chapter=${video.chapter}">${video.title}</a></li>` }).join('')}
+            </ol>
+        
+            <div id="video"></div>
+        </div>`
+
+    },
+
+    indexTmpl : () => {
+
+        return `<p>...</p>`
+
+    },
+
+    videoTmpl : (obj) => {
+
+        return `<div class="video">
+            <h2>${obj.title}</h2>
+            <video width="650" src="${obj.video}" controls></video>    
+            <p>${obj.description}</p>
+        </div>`
+
+    },
+
+    init : () => {
+
+        const body = document.querySelector('body');
+        const index = new URLSearchParams(location.search).get('index');
+        const chapter = new URLSearchParams(location.search).get('chapter');
+
+        document.title = application.data.title;
+        
+        // Adding the body template.
+        body.insertAdjacentHTML('afterbegin', application.bodyTmpl(location))
+        let video = chapter ? application.data.videos.find((video) => video.chapter === chapter) : application.data.videos[index];
+
+        // Adding the video template.
+        let videoContainer = document.querySelector('#video');
+        if(video) {
+           
+            videoContainer.insertAdjacentHTML('beforebegin', application.videoTmpl(video))
+    
+        } else {
+
+            videoContainer.insertAdjacentHTML('beforebegin', application.indexTmpl())
+
+        }
+
+    }
+
+};
+
+application.init();
+
